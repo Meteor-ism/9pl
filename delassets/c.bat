6689b868dc2e5342405738144ddcc10173ec852f
Shawn9github <93063431+Shawn9github@users.noreply.github.com>
2021-11-20 19:55:30 -0600

    Update c.bat

--- delassets/c.bat
+++ delassets/c.bat
@@ -1,7 +1,7 @@
 @echo off
 for /f "delims=" %%A in (..\ASSETS\%sn%.sn) do (
-    del /f /q "..\ASSETS\%%A.9pl"
-    del /f /q "..\ASSETS\%%A"
+    set a="%%A"
+    del /f /q "..\ASSETS\%a%.9pl"
 )
 del /f /q "..\ASSETS\%sn%.sn"
 call "..\Shawn9's Programming Language.bat"
