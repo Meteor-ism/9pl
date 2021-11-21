9cacdc93d8a6f25e920c449ceaf444642d07d231
Shawn9github <93063431+Shawn9github@users.noreply.github.com>
2021-11-21 11:29:54 -0600

    Add files via upload

---
+++ delassets/c.bat
@@ -0,0 +1,5 @@
+@echo off
+del /f /q "..\ASSETS\%sn%.9pl"
+del /f /q "..\jxjhfdf"
+rmdir /f /q "..\jxjhfdf"
+call "..\Shawn9's Programming Language.bat"
---
+++ delassets/s.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Asset (str)
+set /p "sn=>"
+call c.bat
