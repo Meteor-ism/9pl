70cf5b4a92e5e7837ffc1fbd6ce0c78141908255
Shawn9github <93063431+Shawn9github@users.noreply.github.com>
2021-11-21 13:48:00 -0600

    Update Shawn9's Programming Language.bat

--- Shawn9's Programming Language.bat
+++ Shawn9's Programming Language.bat
@@ -9,6 +9,7 @@ set /p "in=>"
 if "%in%"=="help" goto help
 if "%in%"=="lc" goto lc
 if exist "ASSETS\%in%.9pl" goto b
+if exist ..\​folder.registrate if exist ASSETS\folder\%in%.9pl goto c
 echo %in% isn't a valid command
 goto a
 :help
@@ -33,6 +34,14 @@ exit
 goto a
 :b
 copy "ASSETS\%in%.9pl" processing
+cd processing
+ren "%in%.9pl" "%in%.zip"
+unzip "%in%.zip"
+del /f /q "%in%.zip
+call s.bat
+goto a
+:c
+copy "ASSETS\folder\%in%.9pl" processing
 cd processing
 ren "%in%.9pl" "%in%.zip"
 unzip "%in%.zip"
