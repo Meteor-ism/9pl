84384e80d6eaf527b823db2dfe055876cc096846
Shawn9github <93063431+Shawn9github@users.noreply.github.com>
2021-11-20 19:28:46 -0600

    Add files via upload

---
+++ assetsn/c.bat
@@ -0,0 +1,3 @@
+@echo off
+echo %asset% >> "..\ASSETS\%sn%.sn"
+call "..\Shawn9's Programming Language.bat"
---
+++ assetsn/s.bat
@@ -0,0 +1,6 @@
+@echo off
+echo asset (str)
+set /p "asset=>"
+echo serial number (int)
+set /p "sn=>"
+call c.bat
