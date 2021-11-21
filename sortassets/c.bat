dbf931b4bdfa760d6e59a0f45a746a89c9f27ab4
Shawn9github <93063431+Shawn9github@users.noreply.github.com>
2021-11-21 13:35:35 -0600

    Add files via upload

---
+++ sortassets/c.bat
@@ -0,0 +1,20 @@
+@echo off
+if "%CD%"=="0" goto 0
+if "%CD%"=="1" goto 1
+if "%CD%"=="2" goto 2
+:end
+call "..\Shawn9's Programming Language.bat"
+:0
+if exist ..\..\folder.registrate goto end
+md ..\ASSETS\folder
+echo >> ..\..\folder.registrate
+goto end
+:1
+copy ..\ASSETS\folder\*.* ..\ASSETS
+rmdir /s /q ..\ASSETS\folder
+del /f /q ..\..\folder.registrate
+goto end
+:2
+copy ..\ASSETS\%asset%.9pl ..\ASSETS\folder
+del /f /q ..\ASSETS\%asset%.9pl
+goto end
---
+++ sortassets/s.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Create (0)/Delete (1)/Add to folder (2) (0/1/2)
+set /p "CD=>"
+echo asset (ignore if not 2) (str)
+set /p "asset=>"
+call c.bat
