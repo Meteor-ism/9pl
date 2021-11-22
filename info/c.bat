f4171d9ccd0a0f079fbb277501a6a44e7bf49976
Shawn9github <93063431+Shawn9github@users.noreply.github.com>
2021-11-22 12:43:41 -0600

    Add files via upload

---
+++ info/c.bat
@@ -0,0 +1,14 @@
+@echo off
+if "%info%"=="1" goto 1
+if "%info%"=="2" goto 2
+:end
+call "..\Shawn9's Programming Language.bat"
+:1
+echo Hello. I am Shawn9. I created this project by myself. I think that is all I have to say
+echo other than look at my itch.io page for more about me.
+pause
+goto end
+:2
+echo ITCH.IO PAGE YOU LAZY MAN/WOMAN/DOG
+pause
+goto end
---
+++ info/s.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Creator (1)/Program (2) (1/2)
+set /p "info=>"
+call c.bat
